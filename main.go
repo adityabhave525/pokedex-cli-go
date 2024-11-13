b43bd140f19006a248b537a408adb011a4c12355
adityabhave525 <adityabhave525@gmail.com>
2024-11-13 21:18:24 +0000

    Map and Map B functions

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"adityabhave525/pokedexcli/internal/pokeapi"
+	"time"
+)
+
 func main() {
-	startRepl()
+
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeApiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
