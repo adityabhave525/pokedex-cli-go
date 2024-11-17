37b019dca03eafd9f451c1eda5bff997557a8579
adityabhave525 <adityabhave525@gmail.com>
2024-11-17 04:01:21 +0000

    Added pokedex

--- repl.go
+++ repl.go
@@ -68,6 +68,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "List of all caught pokemons",
+			callback:    commandPokedex,
+		},
 		"catch": {
 			name:        "catch <pokemon_name>",
 			description: "Attempt to catch a pokemon",
